d180a14080efddf32f62556864096a83e5c96087
benjamin-bachs <benjamin.bachs@gmail.com>
2024-08-12 20:26:29 -0300

    menu principal mejorado

--- laUltimaCarrera/Program.cs
+++ laUltimaCarrera/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
 
+Console.ForegroundColor = ConsoleColor.Red;
 String title = @"
  _             _   _ _ _   _                   _____                               
 | |           | | | | | | (_)                 /  __ \                              
@@ -10,10 +12,15 @@ String title = @"
                                                                                    
                                                                                    
 ";
-centrarTexto(title);
+CentrarTexto(title);
+Console.ResetColor();
 String s = "Presione cualquier boton para continuar";
-centrarTexto(s);
-Console.ReadLine();
+CentrarTexto(s);
+Console.ReadKey(false);
+Console.Clear();
+MaquinaEscribir("   Un meteorito ha caido en la Facultad de Ciencias Exactas, solo una carrera\n   podra seguir dictándose, pelea para ser la última carrera en pie.");
+ClearBuffer();
+Console.ReadKey(false);
 Console.Clear();
 String menu = @"
    _   _   _   _  
@@ -22,15 +29,69 @@ String menu = @"
   \_/ \_/ \_/ \_/ 
 
 ";
-System.Console.WriteLine(menu);
-System.Console.WriteLine("1. Nueva partida");
-System.Console.WriteLine("2. Cargar partida");
-Console.ReadLine();
 
-static void centrarTexto(String ascii){
+System.Console.WriteLine(menu);
+
+(int left, int top) = Console.GetCursorPosition();
+int option = 1;
+ConsoleKeyInfo key;
+bool isSelected = false;
+
+while (!isSelected)
+{
+	Console.SetCursorPosition(left, top);
+  if(option == 1)
+  {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Nueva partida");
+    Console.ResetColor();
+    Console.WriteLine("Cargar partida");
+  }
+  else
+  {
+    Console.WriteLine("Nueva partida");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Cargar partida");
+    Console.ResetColor();
+  }
+
+	key = Console.ReadKey(false);
+
+	switch (key.Key)
+	{
+		case ConsoleKey.UpArrow:
+			option = option == 1 ? 2 : option - 1;
+			break;
+
+		case ConsoleKey.DownArrow:
+			option = option == 2 ? 1 : option + 1;
+			break;
+
+		case ConsoleKey.Enter:
+			isSelected = true;
+			break;
+	}
+}
+
+static void CentrarTexto(String ascii){
     var lines = ascii.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
     var longestLength = lines.Max(line => line.Length);
     var leadingSpaces = new string(' ', (Console.WindowWidth - longestLength) / 2);
     var centeredText = string.Join(Environment.NewLine, lines.Select(line => leadingSpaces + line));
     Console.WriteLine(centeredText);
+}
+
+static void MaquinaEscribir(string message)
+{
+    for (int i = 0; i < message.Length; i++)
+    {
+        Console.Write(message[i]);
+        System.Threading.Thread.Sleep(60);
+    }
+}
+
+static void ClearBuffer(){
+  while(Console.KeyAvailable){
+    Console.ReadKey(false);
+  }
 }
